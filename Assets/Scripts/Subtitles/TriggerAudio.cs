79c90285f6508241b22f34c3943e1f1cb181fec8
Jean-Baptiste Sanchez <57870849+jeanbapt-sanchez@users.noreply.github.com>
2021-12-10 05:01:48 +0100

    FIX Garland lighting error and ADD random blink on garland

--- Assets/Scripts/Subtitles/TriggerAudio.cs
+++ Assets/Scripts/Subtitles/TriggerAudio.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class TriggerAudio : MonoBehaviour {
     private GameObject activeGameObject;
+    private Coroutine blinkCoroutine;
     public AudioObject clipToPlay;
     
+    
     private void Start() {
-        activeGameObject = GameObject.Find("Garland");
-        activeGameObject.SetActive(false);
+        if (gameObject.tag == "lastEvent") {
+            activeGameObject = GameObject.Find("Garland");
+            activeGameObject.SetActive(false);
+        }
     }
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            if (gameObject.tag == "lastEvent") {
-                activeGameObject.SetActive(true);
-            }
             Vocals.instance.Say(clipToPlay);
+            if (gameObject.tag == "lastEvent") {
+                if(blinkCoroutine != null) {
+                    StopCoroutine(blinkCoroutine);
+                }
+                blinkCoroutine = StartCoroutine(BlinkDelay());
+            }
         }
     }
-
     private void OnTriggerExit(Collider other) {
         GetComponent<BoxCollider>().enabled = false;
+    }
+
+    private IEnumerator BlinkDelay() {
+        System.Random random = new System.Random();
+        float max = 0.9f;
+        float min = 0.1f;
+
+        for (int i = 0; i < 7; i++) {
+            activeGameObject.SetActive(true);
+            yield return new WaitForSeconds((float)random.NextDouble() * (max - min) + min);
+            activeGameObject.SetActive(false);
+            yield return new WaitForSeconds((float)random.NextDouble());
+            activeGameObject.SetActive(true);
+        }
     }
 }
